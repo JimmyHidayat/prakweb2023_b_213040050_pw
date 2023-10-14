38a95f1da73db686126799e4f541a960e290d1ee
JimmyH <96365943+JimmyHidayat@users.noreply.github.com>
2023-10-14 19:56:22 +0700

    Tugas OOP

---
+++ prakweb2023_b_213040050_pw/OOP/Coba.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Coba {
+//     public $a; //Property
+
+//     //Method
+//     public function b() {
+//     }
+// }
+}
+// Mmebuat obeject instance dari class
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+var_dump($a);
+var_dump($b);
+var_dump($c);
+?>
